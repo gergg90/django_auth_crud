a4e52c5053f1b0a120fe3cfde59a9eae155c33d4
GerGG <germain.gg90@gmail.com>
2023-03-06 16:15:40 -0400

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title + ' -By: ' + self.user.username
